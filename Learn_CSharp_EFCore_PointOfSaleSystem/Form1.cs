b8525ed3b5ea97e6afa418b869dc6d3f0f0c7bdc
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-05-10 11:53:13 +0700

    Add project files.

---
+++ Learn_CSharp_EFCore_PointOfSaleSystem/Form1.cs
@@ -0,0 +1,10 @@
+namespace Learn_CSharp_EFCore_PointOfSaleSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Learn_CSharp_EFCore_PointOfSaleSystem/Program.cs
@@ -0,0 +1,17 @@
+namespace Learn_CSharp_EFCore_PointOfSaleSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
